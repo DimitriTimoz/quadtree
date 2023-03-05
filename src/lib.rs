5487a708bfd1edd9309f3987f7b3852a95373a28
Dimitri <dimitri.timoz@orange.fr>
2023-03-05 14:12:54 +0100

    trait Number

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
 pub mod coordinate;
 pub mod box2d;
 pub mod prelude;
+pub mod quadtree;
+
+pub trait Number: std::ops::Sub<Output = Self> + std::ops::Add<Output = Self> + std::ops::Div<Output = Self> + std::cmp::PartialOrd + Copy + std::convert::From<u8> {}
+
+impl Number for f32 {}
+
+impl Number for f64 {}
+
+impl Number for i16 {}
+
+impl Number for i32 {}
+
+impl Number for i64 {}
+
+impl Number for i128 {}
+
+impl Number for isize {}
+
+impl Number for u8 {}
+
+impl Number for u16 {}
+
+impl Number for u32 {}
+
+impl Number for u64 {}
+
+impl Number for u128 {}
+
+impl Number for usize {}
+
+
